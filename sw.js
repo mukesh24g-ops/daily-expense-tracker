e3f092872f903a9bb6df4272daf94837f1e2371f
mukesh24g-ops <mukesh24g@gmail.com>
2026-09-14 13:00:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 
-const CACHE="expense-tracker-pwa-v1";
+const CACHE="expense-tracker-pwa-v3";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET")return;
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{caches.open(CACHE).then(c=>c.put(e.request,n.clone()));return n}).catch(()=>caches.match("./index.html"))));
-});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{caches.open(CACHE).then(c=>c.put(e.request,n.clone()));return n}).catch(()=>caches.match("./index.html"))))});
